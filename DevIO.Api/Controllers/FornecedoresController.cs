d9679f6ccf05287b572efa4735556009f1e9ff2b
Wesley Alves <wesley.alves@sete.inf.br>
2024-01-16 12:45:30 -0300

    Implementação para ObterPorId

--- DevIO.Api/Controllers/FornecedoresController.cs
+++ DevIO.Api/Controllers/FornecedoresController.cs
@@ -16,11 +16,26 @@ namespace DevIO.Api.Controllers
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
         }
+
         [HttpGet]
         public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
         {
-            var fornecedore = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
-            return fornecedore;
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return fornecedor;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if(fornecedor == null ) return NotFound();    
+            
+            return fornecedor;
+        }
+
+        public async  Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id){
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
         }
     }
 }
