2d5e0b59498e798fcc8f0203b41a06006921b6d2
Wesley Alves <wesley.info07@gmail.com>
2024-01-16 21:49:38 -0300

    Injeção de dependencia

--- DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,7 @@
 
 using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 
@@ -10,7 +12,13 @@ namespace DevIO.Api.Configuration
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
             services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IProdutoService, ProdutoService>();
 
             return services;
         }
