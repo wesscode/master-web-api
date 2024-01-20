bcf986a84ce925c2d4f633e2968f1f98b5e9965d
Wesley Alves <wesley.info07@gmail.com>
2024-01-20 20:41:06 -0300

    endpoint de atualizar produto

--- DevIO.Api/Controllers/ProdutosController.cs
+++ DevIO.Api/Controllers/ProdutosController.cs
@@ -55,6 +55,42 @@ namespace DevIO.Api.Controllers
             return CustomResponse(produtoViewModel);
         }
 
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> Atualizar(Guid id, ProdutoViewModel produtoViewModel)
+        {
+            if (id != produtoViewModel.Id)
+            {
+                NotificarErro("O id informado não é o mesmo que foi passado na query.");
+                return CustomResponse(produtoViewModel);
+            }
+
+            var produtoAtualizacao = await ObterProduto(id);
+            produtoViewModel.Imagem = produtoAtualizacao.Imagem;
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            if (produtoViewModel.ImagemUpload != null)
+            {
+                var imagemNome = Guid.NewGuid() + "_" + produtoViewModel.Imagem;
+                if (!UploadArquivo(produtoViewModel.ImagemUpload, imagemNome))
+                {
+                    return CustomResponse(ModelState);
+                }
+                produtoAtualizacao.Imagem = imagemNome;
+            }
+
+            produtoAtualizacao.FornecedorId = produtoViewModel.FornecedorId;
+            produtoAtualizacao.Nome = produtoViewModel.Nome;
+            produtoAtualizacao.Descricao = produtoViewModel.Descricao;
+            produtoAtualizacao.Valor = produtoViewModel.Valor;
+            produtoAtualizacao.Ativo = produtoViewModel.Ativo;
+
+            var produto = _mapper.Map<Produto>(produtoAtualizacao);
+            await _produtoService.Adicionar(produto);
+
+            return CustomResponse(produtoViewModel);
+        }
+
         [HttpDelete]
         public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id)
         {
