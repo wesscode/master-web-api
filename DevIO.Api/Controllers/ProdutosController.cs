03df5219c7e8b8a8459fc33b8a640d279100b2b3
Wesley Alves <wesley.info07@gmail.com>
2024-01-20 17:12:09 -0300

    Ajuste validação.

--- DevIO.Api/Controllers/ProdutosController.cs
+++ DevIO.Api/Controllers/ProdutosController.cs
@@ -70,13 +70,13 @@ namespace DevIO.Api.Controllers
 
         private bool UploadArquivo(string arquivo, string imgNome)
         {
-            var imageDataByteArray = Convert.FromBase64String(arquivo);
             if (string.IsNullOrEmpty(arquivo))
             {
                 NotificarErro("Forneca uma imagem para este produto!");
                 return false;
             }
 
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
 
             if (System.IO.File.Exists(filePath))
