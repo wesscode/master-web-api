99553ea5e5bdfbc6f0b870fe6669302f5aa3a842
Wesley Alves <wesley.info07@gmail.com>
2024-01-20 21:00:31 -0300

    Otimizando a classe Program

---
+++ DevIO.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfig(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true; //desabilita as formas de validações autómaticas das viewmodels 
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    );
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfig(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("Development");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            
+
+            return app;
+        }
+    }
+}
--- DevIO.Api/Program.cs
+++ DevIO.Api/Program.cs
@@ -7,53 +7,22 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Services
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
 builder.Services.AddAutoMapper(typeof(Program));
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true; //desabilita as formas de validações autómaticas das viewmodels 
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Development",
-        builder => builder
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        );
-});
-
+builder.Services.AddApiConfig();
 builder.Services.ResolveDependencies();
 
+//Configure
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("Development");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseApiConfig(app.Environment);
 app.MapControllers();
-
 app.Run();
