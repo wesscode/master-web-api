8e19efef3e6e0d7ce49146731b3715ba84cc67a2
Wesley Alves <wesley.info07@gmail.com>
2024-01-26 22:27:53 -0300

    Definindo configurações do token

--- DevIO.Api/Configuration/IdentityConfig.cs
+++ DevIO.Api/Configuration/IdentityConfig.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using DevIO.Api.Data;
 using DevIO.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DevIO.Api.Configuration
 {
@@ -19,6 +22,33 @@ namespace DevIO.Api.Configuration
             .AddEntityFrameworkStores<ApplicationDbContext>() //indica o contexto
             .AddErrorDescriber<IdentityMensagensPortugues>()
             .AddDefaultTokenProviders(); //habilita uso de token para recuperação de emails.
+
+            // JWT
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; //padrão de auth
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; //como resolver pra validar o token
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true; //garantia que so pode trabalhar com https
+                x.SaveToken = true; //salva o token internamente, melhorando para validações posteriores.
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true, //
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
 
             return services;
         }
